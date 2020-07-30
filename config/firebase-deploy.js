8691b5fb25f91f43c2e040e7a53e8865f5d103ba
David Asher <dasher@computer.org>
2020-07-30 08:37:26 -0400

    fixup firebase-deploy.js

--- config/firebase-deploy.js
+++ config/firebase-deploy.js
@@ -1,16 +1,27 @@
 /*
  * firebase-deploy.js
+ * Copyright (c) 2019-2020 by David Asher, https://github.com/david-asher
+ * 
+ * This script is run from the project root "npm run deploy"
+ * and performs firebase deploy for database and storage security rules
+ * as well as cloud function deploy
  */
 
 const { execSync } = require( 'child_process' )
 const { exit } = require('process')
+const { writeFileSync, readFileSync }  = require('fs')
 
 const fbrcFile = "./.firebaserc"
 const fbDeployFile = "./firebase.json"
 
 function writeJson( filePath, jsonContent )
 {
-    fs.writeFileSync( filePath, JSON.stringify( jsonContent, null, 2 ) )
+    writeFileSync( filePath, JSON.stringify( jsonContent, null, 2 ) )
+}
+
+function readJson( filePath )
+{
+    return JSON.parse( readFileSync( filePath ) )
 }
 
 function fbDeploy( command )
@@ -18,16 +29,14 @@ function fbDeploy( command )
     execSync( "firebase deploy --only " + command )
 }
 
-const fbConfig = require( './config/firebase-config.json' )
-console.log( "bucket = ", fbConfig.storageBucket )
+const fbConfig = readJson( './firebase-config.json' )
 if ( !fbConfig.storageBucket ) {
-    console.log( "ERROR: firebase-config.json must be configured for your firebase project." )
+    console.error( "ERROR: firebase-config.json must be configured for your firebase project." )
     exit(10)
 }
 
 console.log( "** configure .firebaserc file" )
-var fbrcJson = require( fbrcFile )
-console.log( "fbrcJson = ", fbrcJson )
+var fbrcJson = readJson( fbrcFile )
 fbrcJson.projects.default = fbConfig.projectId
 writeJson( fbrcFile, fbrcJson )
 
@@ -35,11 +44,11 @@ console.log( "** deploy firestore:rules" )
 fbDeploy( "firestore:rules" )
 
 console.log( "** deploy storage:rules" )
-const fbDeployJson = require( fbDeployFile )
-console.log( "fbDeployJson = ", fbDeployJson )
-fbDeployJson.storage.bucket = fbConfig.storageBucket
+const fbDeployJson = readJson( fbDeployFile )
+fbDeployJson.storage[0].bucket = fbConfig.storageBucket
 writeJson( fbDeployFile, fbDeployJson, null )
 fbDeploy( "storage:rules" )
 
 console.log( "** deploy firebase functions" )
+execSync( "npm install", { cwd: "./functions" } )
 fbDeploy( "functions" )

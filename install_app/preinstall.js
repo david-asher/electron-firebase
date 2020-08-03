b5fd784c5c0c4c3d120b2a6daa96c5f07f4c5528
David Asher <dasher@computer.org>
2020-08-03 10:47:45 -0400

    node-gyp is a pre-condition

--- install_app/preinstall.js
+++ install_app/preinstall.js
@@ -5,7 +5,8 @@
  */
 'use strict';
 
-const { execSync } = require( 'child_process' )
+const { execSync } = require( 'child_process' );
+const { exit } = require('process');
 
 function checkCommand( commandString )
 {
@@ -23,6 +24,18 @@ process.env.npm_config_loglevel = "error"
 
 console.log( "Please be patient, electron and firebase are large projects and installation may take a few minutes." )
 
+// check for command existence before installing node-gyp
 if ( !checkCommand( 'node-gyp' ) ) {
     execSync( "npm install -g node-gyp" )
 }
+// if we did not have have permission, install would have failed, so try again as su
+if ( !checkCommand( 'node-gyp' ) ) {
+    execSync( "sudo npm install -g node-gyp" )
+}
+// if all of this failed, stop, because we can't build without node-gyp
+if ( !checkCommand( 'node-gyp' ) ) {
+    console.error( "Cannot find node-gyp and failed to install it. " )
+    console.error( "Please check permissions and try to install node-gyp yourself before proceding." )
+    exit(23)
+}
+
